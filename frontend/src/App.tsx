1f3b97ce3d02baffce63a5c9667a57efec472629
Aura <auratook@gmail.com>
2022-03-01 19:44:58 +0530

    feat(react): scrap all prior code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import './App.css';
-import Blobs from './components/blobs';
-import HeadText from './components/headText';
 
 function App() {
   return (
     <div className="App">
-      <Blobs />
-      <HeadText />
     </div>
   );
 }
