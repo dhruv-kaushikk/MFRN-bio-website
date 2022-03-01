e78837fbdb3e0c16d6d11053493af59256a3fd8a
Aura <auratook@gmail.com>
2022-03-01 18:16:13 +0530

    feat(frontend): Add a landing page

---
+++ frontend/src/components/headText.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './headText.css'
+type Props = {}
+
+function headText({ }: Props) {
+	return (
+		<div className='headText'>
+			<h1>I am a Developer</h1>
+		</div>
+	)
+}
+
+export default headText
