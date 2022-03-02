89d524965df6621ae6c4027ec9fd2bfa17edc00c
Aura <auratook@gmail.com>
2022-03-02 19:17:00 +0530

    feat(react): add soothing text on landing page

--- frontend/src/components/landingPage/headers.tsx
+++ frontend/src/components/landingPage/headers.tsx
@@ -1,14 +1,27 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 import TypewriterComponent from 'typewriter-effect'
 import './headers.css'
 const MainText = () => {
     return (
         <div className="main-text">
-            <TypewriterComponent
-                onInit={(typewriter) => {
-                    typewriter.typeString('I am a developer').start()
-                }}
-            />
+            <Typography variant="h1" className="main-text-title">
+                Hi, I am dhruv kaushik.
+            </Typography>
+            <Typography variant="h2" className="main-text-typography">
+                <TypewriterComponent
+                    onInit={(typewriter) => {
+                        typewriter
+                            .typeString('A Web Developer.')
+                            .pauseFor(2000)
+                            .deleteAll(75)
+                            .typeString('A Backend Developer.')
+                            .pauseFor(2000)
+                            .start()
+                    }}
+                    options={{ loop: true, deleteSpeed: 75 }}
+                />
+            </Typography>
         </div>
     )
 }
