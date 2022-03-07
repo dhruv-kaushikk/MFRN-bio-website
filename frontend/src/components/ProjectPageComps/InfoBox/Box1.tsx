e00067addeada23f39652ae10ebdfa0f40087c74
Aura <auratook@gmail.com>
2022-03-07 19:47:21 +0530

    fix(landingPage): remove unused var

--- frontend/src/components/ProjectPageComps/InfoBox/Box1.tsx
+++ frontend/src/components/ProjectPageComps/InfoBox/Box1.tsx
@@ -1,10 +1,9 @@
-import { Box } from "@mui/material"
 import './box1.css'
 function box1() {
-	return (
-		<div className="centerText">
-			<h1 >IMAGINE A DEVELOPER...</h1>
-		</div>
-	)
+    return (
+        <div className="centerText">
+            <h1>IMAGINE A DEVELOPER...</h1>
+        </div>
+    )
 }
 export default box1
